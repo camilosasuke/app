1db07f68896bee02721d77cb06f3e6374876110d
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2017-07-27 14:44:48 -0300

    Windows Home Run!! From bilbo-macos!! bilbo and Mummi contribution!! veriverse contribution!! https://LiveEdu.tv/ (Livecoding.tv) contribution!! ThomasBS (Thomas Boregaard Sørensen, https://kaem.dk/, https://kaem.io/, tbhs1000%💛for∞growing!! Den bedste udvikler!), drmjg, camwhite (Cameron White, Twitchs' scamwhite), xmetrix, (CatherinaCortega, Twitchs' helenawinter), fuel, r00tkiller, hiiamgosu (itashellhaha...(gosu) ), pablosj (sjpablo, Pawel Kulesza, Pablosj special contribution (after so much days with so low audience... ) .. contribution!!), phanxgames, anita, bill_gates, costinis, intelijens, denvercowboy, dslegends, ederangelo21..., epryka, guacjack, georgeb, gigaherz, hosjenken, intelijens, irwanbd, jaycee, kaeris, karrmarr, kissloryshy, kozmo. kranalex, mark_miller, masterchoc, matroskinb, novicehere, olivert, pirateofthesea, polodonald, rex2, riverside, samdev, siox, starlord91, stefan_v, systemis, thiagovgamg, tonycode, valdinei, vapaamies, vodkavodkavodka228, wetriks, yanaseattle and zzzel contribution!! https://Twitch.tv/ contribution!! ! ThomasBS_ (LiveEdu.tv ThomasBS, Den besten Software udvikler), AlfredoPlays (and his mates... Alfredo, Hanson and Mackie contribution), RocketBeans.TV (Nils, Simon, Colin and Etienne), Peter_Alfons_Loch (from phanxgames streamd and discord channel, first "explicit" and "spontaneous" commit and contribution to this newly create GitHub repository), Spoofh (LiveEdu.tv Fuel: Benjamin Kuhl), themushmouth (LiveEdu.tv mushmouth: Patrick McFarling), Tectuma (Kevin, Stacie (GothicCat1), Matt Brock (Cronos) and Dylan and Nick...), ilomoo, helenawinter (CatherinaHelena, CatherinaCortega from LiveEdu) GameDuells, OrcaPrime, OxenHill, Pixel701, recursionishard, theformemrcc, XanderGamingTwich and xmxstudio (LiveCoding/LiveEdu xmetrix) contribution!! https://mixer.com/ (previous Beam.pro) contribution!! ArrestedMoney2 contribution!! bisba contribution!! Carol, Lúcio and Lucinho contribution!!
    from bilbo at veriverse!! Internet Net Curitiba 60Mbps▼ 6Mbps▴
    i7 6700 Dell XPS 8900 32GB DDR4 RAM 2x240GB SSD 2TB HDD
    Windows 10 with Hyper-V (quad boot with Windows 10 Pro, Ubuntu 17.04 and macOS Sierra)

--- appseed/sphere/sphere/sphere_application.cpp
+++ appseed/sphere/sphere/sphere_application.cpp
@@ -91,6 +91,10 @@ namespace sphere
 
    int32_t application::exit_application()
    {
+
+
+      System.factory().creatable_small < main_frame >();
+
       string strId = m_strId;
       char chFirst = '\0';
       if(strId.get_length() > 0)
