a3ae45240b80b7371640296a68619ea544430dd5
Camilo Sasuke Tsumanuma <camilo@ca2.email>
2017-07-27 14:19:42 -0300

    Windows Home Run!! From bilbo-macos!! bilbo and Mummi contribution!! veriverse contribution!! https://LiveEdu.tv/ (Livecoding.tv) contribution!! ThomasBS (Thomas Boregaard Sørensen, https://kaem.dk/, https://kaem.io/, tbhs1000%💛for∞growing!! Den bedste udvikler!), drmjg, camwhite (Cameron White, Twitchs' scamwhite), xmetrix, (CatherinaCortega, Twitchs' helenawinter), fuel, r00tkiller, hiiamgosu (itashellhaha...(gosu) ), pablosj (sjpablo, Pawel Kulesza, Pablosj special contribution (after so much days with so low audience... ) .. contribution!!), phanxgames, bill_gates, costinis, intelijens, denvercowboy, dslegends, ederangelo21..., epryka, guacjack, georgeb, gigaherz, hosjenken, intelijens, irwanbd, jaycee, kaeris, karrmarr, kissloryshy, kozmo. kranalex, mark_miller, masterchoc, matroskinb, novicehere, olivert, pirateofthesea, polodonald, rex2, riverside, samdev, siox, starlord91, stefan_v, systemis, thiagovgamg, tonycode, valdinei, vapaamies, vodkavodkavodka228, wetriks, yanaseattle and zzzel contribution!! https://Twitch.tv/ contribution!! ! ThomasBS_ (LiveEdu.tv ThomasBS, Den besten Software udvikler), AlfredoPlays (and his mates... Alfredo, Hanson and Mackie contribution), RocketBeans.TV (Nils, Simon, Colin and Etienne), Peter_Alfons_Loch (from phanxgames streamd and discord channel, first "explicit" and "spontaneous" commit and contribution to this newly create GitHub repository), Spoofh (LiveEdu.tv Fuel: Benjamin Kuhl), themushmouth (LiveEdu.tv mushmouth: Patrick McFarling), Tectuma (Kevin, Stacie (GothicCat1), Matt Brock (Cronos) and Dylan and Nick...), ilomoo, helenawinter (CatherinaHelena, CatherinaCortega from LiveEdu) GameDuells, OrcaPrime, OxenHill, Pixel701, recursionishard, theformemrcc, XanderGamingTwich and xmxstudio (LiveCoding/LiveEdu xmetrix) contribution!! https://mixer.com/ (previous Beam.pro) contribution!! ArrestedMoney2 contribution!! bisba contribution!! Carol, Lúcio and Lucinho contribution!!
    from bilbo at veriverse!! Internet Net Curitiba 60Mbps▼ 6Mbps▴
    i7 6700 Dell XPS 8900 32GB DDR4 RAM 2x240GB SSD 2TB HDD
    Windows 10 with Hyper-V (quad boot with Windows 10 Pro, Ubuntu 17.04 and macOS Sierra)

---
+++ appseed/sphere/sphere/sphere_main_frame.cpp
@@ -0,0 +1,237 @@
+#include "framework.h"
+
+
+namespace sphere
+{
+
+
+   main_frame::main_frame(::aura::application * papp) :
+      ::object(papp),
+      simple_frame_window(papp)
+   {
+
+      WfiEnableFullScreen();
+
+   }
+
+
+   main_frame::~main_frame()
+   {
+
+   }
+
+
+   bool main_frame::get_translucency(::user::ETranslucency & etranslucency)
+   {
+
+      etranslucency = ::user::TranslucencyPresent;
+
+      return true;
+
+   }
+
+
+   bool main_frame::get_color(COLORREF & cr, ::user::e_color ecolor)
+   {
+
+      bool bOk = true;
+
+      if (ecolor == ::user::color_text || ecolor == ::user::color_edit_text)
+      {
+
+         cr = Application.get_theme_fg_color();
+         //if (Application.get_theme() == theme_dark)
+         //{
+
+         //   cr = ARGB(255, 250, 250, 250);
+
+         //}
+         //else if (Application.get_theme() == theme_blue)
+         //{
+
+         //   cr = ARGB(255, 0, 0, 0);
+
+         //}
+         //else
+         //{
+
+         //   cr = ARGB(255, 0, 0, 0);
+
+         //}
+
+      }
+      else if (ecolor == ::user::color_text_selected)
+      {
+
+         cr = ARGB(255, 255, 255, 255);
+
+      }
+      else if (ecolor == ::user::color_text_highlight)
+      {
+
+         cr = ARGB(255, 55, 105, 255);
+
+      }
+      else if (ecolor == ::user::color_text_selected_highlight)
+      {
+
+         cr = ARGB(255, 102, 153, 255);
+
+      }
+      else if (ecolor == ::user::color_background || ecolor == ::user::color_edit_background)
+      {
+
+         //if (Application.get_theme() == theme_dark)
+         //{
+
+         //   if (_001IsTranslucent())
+         //   {
+
+         //      cr = ARGB(184, 55, 55, 55);
+
+         //   }
+         //   else
+         //   {
+
+         //      cr = ARGB(255, 55, 55, 55);
+
+         //   }
+
+         //}
+         //else if (Application.get_theme() == theme_blue)
+         //{
+
+         //   if (_001IsTranslucent())
+         //   {
+
+         //      cr = ARGB(184, 55, 155, 255);
+
+         //   }
+         //   else
+         //   {
+
+         //      cr = ARGB(255, 55, 155, 255);
+
+         //   }
+
+         //}
+         //else
+         //{
+
+         //   if (_001IsTranslucent())
+         //   {
+
+         //      cr = ARGB(184, 255, 255, 255);
+
+         //   }
+         //   else
+         //   {
+
+         //      cr = ARGB(255, 255, 255, 255);
+
+         //   }
+
+
+         //}
+
+         cr = Application.get_theme_bg_color();
+
+      }
+      else if (ecolor == ::user::color_background_selected)
+      {
+
+         if (_001IsTranslucent())
+         {
+
+            cr = ARGB(184, 0, 0, 127);
+
+         }
+         else
+         {
+
+            cr = ARGB(255, 0, 0, 127);
+
+         }
+
+      }
+      else if (ecolor == ::user::color_background_selected)
+      {
+
+         if (_001IsTranslucent())
+         {
+
+            cr = ARGB(184, 0, 0, 127);
+
+         }
+         else
+         {
+
+            cr = ARGB(255, 0, 0, 127);
+
+         }
+
+      }
+      else if (ecolor == ::user::color_border_normal)
+      {
+
+         cr = ARGB(255, 84, 84, 77);
+
+      }
+      else if (ecolor == ::user::color_border_hover)
+      {
+
+         cr = ARGB(255, 84, 184, 77);
+
+      }
+      else if (ecolor == ::user::color_text_hover)
+      {
+
+         cr = ARGB(255, 84, 184, 255);
+
+      }
+      else
+      {
+
+         bOk = false;
+
+      }
+
+      return bOk;
+
+   }
+
+
+
+} // namespace asphere
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ appseed/sphere/sphere/sphere_main_frame.h
@@ -0,0 +1,45 @@
+#pragma once
+
+
+namespace sphere
+{
+
+
+   class CLASS_DECL_SPHERE main_frame :
+      public simple_frame_window
+   {
+   public:
+
+
+      main_frame(::aura::application * papp);
+      virtual ~main_frame();
+
+
+      virtual bool get_translucency(::user::ETranslucency & etranslucency) override;
+
+      virtual bool get_color(COLORREF & cr, ::user::e_color ecolor) override;
+
+
+   };
+
+
+} // namespace sphere
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
